5f9395317f515e4265bd47cc9066a8292e12f15f
ashwani2529 <ashwanix2749@gmail.com>
2024-10-07 01:17:52 +0530

    update URLs

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 5000;
 app.use(cors(
   {
-    origin: "https://mern-dashboard-lkib.onrender.com",
+    origin: "https://mern-dashboard-posts.netlify.app",
     credentials: true
   }
 ));
--- frontend/src/components/signup/Signup.jsx
+++ frontend/src/components/signup/Signup.jsx
@@ -1,11 +1,10 @@
-import { React, useEffect, useState } from "react";
+import { React, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   Avatar,
   Button,
   CssBaseline,
   TextField,
-  MenuItem,
   Link,
   Grid,
   Box,
